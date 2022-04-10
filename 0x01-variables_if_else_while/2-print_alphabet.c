734cd47365a24a4ad140be91842552dfbbf9210b
RuthNaibei <ruthnaibei2000@gmail.com>
2022-04-09 23:36:02 -0700

    prints programs in lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,21 @@
+
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ *  * main - main block
+ *   * Description: Get a random number and check its last digit, compare it with 5
+ *    * Return: 0
+ *     */
 int main(void)
 {
+		char c = 'a';
 
-	char c;
+			while (c <= 'z')
+					{
+								putchar(c);
+										c++;
+											}
 
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-		putchar('\n');
-
-return (0);
+				putchar('\n');
+					return (0);
 }
